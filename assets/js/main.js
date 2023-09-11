e8cceee78a2f7ed5a400fceb1d39b572c9dc99cc
Manuel Adán Carrillo Zavala <manuelcarrillo.zavala@agenciamuza.com>
2023-09-11 11:43:57 -0600

    #865d2fn8h Se integran los cambios para la búsqueda de coincidencias en las descripciones

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@ const productContainer = document.querySelector('#product-container');
 fetch(url).then(response => response.json()).
 then(
     (data) => {
-    const products = data.productos.slice(0, 10);
+    const products = data.productos.slice(0, 20);
 
     products.forEach(product => {
         const datosProducto = {
@@ -107,4 +107,122 @@ then(
         productContainer.innerHTML += templateString;
         productContainer.innerHTML += modalTemplateString;
     });
+
+    let filterProducts = data.productos;
+    const txt_search = document.querySelector('#txt-search');
+
+    document.querySelector('#btn-search').addEventListener('click', () => {
+        let res = filterProducts.filter(product => {
+            return product.nombre.includes(txt_search.value);
+        });
+        if(res.length > 0) {
+            const productContainer = document.querySelector('#product-container');
+            while(productContainer.firstChild) {
+                productContainer.removeChild(productContainer.firstChild);
+            }
+        }
+        res.forEach(item => {
+            const datosProducto = {
+                id: item.id,
+                nombre: item.nombre,
+                descripcion: item.descripcion,
+                precio: parseFloat(item.precio),
+                materiales: item.materiales,
+                funcionalidad: item.funcionalidad,
+                cuidados: item.cuidados,
+                imagen_frontal: item.imagen_frontal,
+                imagen_lateral : item.imagen_lateral,
+                imagen_extra: item.imagen_general,
+            }
+
+            const templateString = 
+        `
+        <div class="col-12 col-md-6 col-lg-4 mb-4">
+            <div class="card">
+                <img class="card-img-top" src="${datosProducto.imagen_frontal}" alt="..."/>
+                <div class="card-body">
+                    <div class="d-flex flex-row">
+                        <h5 class="card-title text-truncate">${datosProducto.nombre}</h5>
+                        <a id="salas" class="ml-auto card-title" href="#"></a>
+                    </div>
+                    <p class="card-text text-truncate" style="max-height: 200px">
+                        ${datosProducto.descripcion}
+                    </p>
+                    <input class="btn btn-dark btn-block" type="button" value="Saber más" data-toggle="modal" data-target="#${datosProducto.id}exampleModal"/>
+                </div>
+            </div>
+        </div>
+        `;
+
+        const modalTemplateString = 
+        `
+        <div class="modal fade bd-example-modal-lg" id="${datosProducto.id}exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-lg" role="document">
+              <div class="modal-content">
+                <div class="modal-header bg-dark text-light">
+                  <h5 class="modal-title" id="exampleModalLabel">${datosProducto.nombre}</h5>
+                  <button type="button" class="close text-light" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                    <div class="row align-items-center">
+                        <div class="col-12 col-md-6">
+                            <img class="img-fluid" src="${datosProducto.imagen_frontal}" alt="..."/>
+                        </div>
+                        <div class="col-12 col-md-6">
+                            <table class="table table-striped">
+                                <thead class="thead-dark">
+                                    <tr>
+                                        <th scope="col">
+                                            <div>Precio</div>
+                                        </th>
+                                        <th scope="col">
+                                            <div >Funcionalidad</div>
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>$${datosProducto.precio}</td>
+                                        <td>${datosProducto.funcionalidad}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            <p>${datosProducto.materiales}</p>
+                            <p>${datosProducto.cuidados}</p>
+                            <input class="btn btn-block btn-outline-dark" type="button" value="Agregar al carrito"/>
+                        </div>
+                        
+                        <div class="d-flex flex-lg-row flex-column justify-content-around">
+                            <div class="p-1">
+                                <img class="w-100 h-auto" src="${datosProducto.imagen_frontal}" alt="">
+                            </div>
+                            <div class="p-1">
+                                <img class="w-100 h-auto" src="${datosProducto.imagen_lateral}" alt="">
+                            </div>
+                            <div class="p-1">
+                                <img class="w-100 h-auto" src="${datosProducto.imagen_extra}" alt="">
+                            </div>
+                        </div>
+
+                    </div>
+                    <p>
+                        ${datosProducto.descripcion}
+                    </p>
+                </div>
+                <div class="modal-footer bg-dark text-light">
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                </div>
+              </div>
+            </div>
+        </div>
+        `;
+
+        productContainer.innerHTML += templateString;
+        productContainer.innerHTML += modalTemplateString;
+        });
+    });
+
+    
 });
